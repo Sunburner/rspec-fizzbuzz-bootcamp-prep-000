5575c14e1beacf38d61497e6b5591b3317d2d4a4
Angelo Cigna <acigna1982@gmail.com>
2018-02-07 01:04:51 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,12 @@
-def fizzbuzz
-
+def fizzbuzz(int)
+  if int % 3 == 0
+    puts 'fizz'
+  elsif
+    if int % 5 == 0
+      puts 'buzz'
+  elsif
+    if int % 15 == 0
+      puts 'fizzbuzz'
+    else
+      puts 'nil'
 end
